62cd760fa6b4ff6675286e84ed376a1dc1c7b649
MarkGorlevskey <124619894+MarkGorlevskey@users.noreply.github.com>
2023-08-11 11:44:24 +0300

    Delete task-3.1.5_js/PP_3_1_5_js directory

--- task-3.1.5_js/PP_3_1_5_js/src/main/java/ru/kata/spring/boot_security/demo/exeption/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.exeption;
-
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public class UserNotFoundException extends UsernameNotFoundException {
-    public UserNotFoundException(String msg) {
-        super(msg);
-    }
-}
